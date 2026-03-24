d5e7ca23eab89eb73edc6dffe17e71c388a6e199
octojon <mail@octojon.dev>
2026-03-24 11:31:25 -0400

    Added Processing boilerplate

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public void settings() {
+      size(400, 400);
+    }
+    public void draw() {
+      background(255);
+      circle(width/2, height/2, 50);
+    }
+
   public static void main(String[] args) {
-    System.out.println("Hi!");
+    String[] processingArgs = {"MySketch"};
+		Main mySketch = new Main();
+		PApplet.runSketch(processingArgs, mySketch);
   }
 }
