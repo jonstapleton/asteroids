0f16e2af766e9488b116c08a75dcc5952709032f
Gzero28 <alex.perry2801@gmail.com>
2026-03-26 11:14:53 -0400

    First code instance

--- Main.java
+++ Main.java
@@ -2,12 +2,25 @@ import processing.core.PApplet;
 
 public class Main extends PApplet {
 
+  Asteroid a;
+  Asteroid b;
+  Asteroid c;
+
     public void settings() {
-      size(400, 400);
+      size(800, 800);
     }
+
+    public void setup() {
+      a = new Asteroid(Asteroid.Size.LARGE, this);
+      b = new Asteroid(Asteroid.Size.MEDIUM, this);
+      c = new Asteroid(Asteroid.Size.SMALL, this);
+    }
+
     public void draw() {
       background(255);
-      circle(width/2, height/2, 50);
+      a.Display(this);
+      b.Display(this);
+      c.Display(this);
     }
 
   public static void main(String[] args) {
