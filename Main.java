dc50c20fbaba036038a07fbede5ab49c0213de00
G01NGP0ZT4L <xxg01ngp0zt4lxx@gmail.com>
2026-03-26 11:38:03 -0400

    edited main
    added spaceship code to the main
    removed test circle
    score still doesnt show

--- Main.java
+++ Main.java
@@ -1,13 +1,34 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet {
+  
+PImage img1;
+PImage img2;
+PImage img3;
+float odd = 0;
 
     public void settings() {
       size(400, 400);
-    }
+      img1 = loadImage("./frame1.png");
+      img2 = loadImage("./frame2.png");
+      img3 = loadImage("./still.png");
+}
+    
     public void draw() {
       background(255);
-      circle(width/2, height/2, 50);
+      image(img3, 125, 125);
+      if (keyCode == UP){
+        odd = odd + 0.3f;
+        PImage img;
+      if (floor(odd) % 2 == 0) {
+        img = img1;
+  }
+      else {
+        img = img2;
+  }
+  image(img, 125, 125);
+ }
     }
 
   public static void main(String[] args) {
--- spaced_ship.java
+++ spaced_ship.java
@@ -8,18 +8,18 @@ PImage img2;
 PImage img3;
 float odd = 0;
 
-void setup() {
+public void setup() {
   size (400,400);
   img1 = loadImage("./frame1.png");
   img2 = loadImage("./frame2.png");
   img3 = loadImage("./still.png");
 }
  
-void draw() {
+public void draw() {
     background(255);
     image(img3, 0, 0);
  if (keyCode == UP){
-  odd = odd + 0.3;
+  odd = odd + 0.3f;
   PImage img;
   if (floor(odd) % 2 == 0) {
     img = img1;
