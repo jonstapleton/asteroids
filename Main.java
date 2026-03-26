4e2ba8c5697d5dcb97d173bc132584a8d91169ee
zainabkamran-gif <zainab.kamran@emu.edu>
2026-03-26 11:50:39 -0400

    Removed Hud

--- Main.java
+++ Main.java
@@ -1,18 +1,37 @@
 import processing.core.PApplet;
 
-public class Main extends PApplet {
+public class Main extends PApplet{
 
-    public void settings() {
-      size(400, 400);
+  Projectile[] proj;
+  int i = 1;
+
+  public void settings() {
+    size(400,400);
+  }
+
+  public void setup() {
+    //proj = new Projectile();
+    proj = new Projectile[10];
+    proj[0] = new Projectile(this);
+  }
+
+  public void draw() {
+    background(255);
+    ellipse(200,200,40,40);
+  //proj[0].display();
+    for(int index=0;index<i;index++) {
+      proj[index].display(this);
     }
-    public void draw() {
-      background(255);
-      circle(width/2, height/2, 50);
+    
+  if(keyPressed && key == 'z' && frameCount%6==0) {
+      proj[i] = new Projectile(this);
+      i = (i + 1)%10;
     }
+  }
 
   public static void main(String[] args) {
-    String[] processingArgs = {"MySketch"};
-		Main mySketch = new Main();
-		PApplet.runSketch(processingArgs, mySketch);
+    String[] processingArgs = { "MySketch" };
+    Main mySketch = new Main();
+    PApplet.runSketch(processingArgs, mySketch);
   }
 }
