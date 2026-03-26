7ebd93f1ee7d0cb1af7ff8c657540bfc79cd0621
BenTheBomb <65471744+BenTheBomb@users.noreply.github.com>
2026-03-26 11:13:10 -0400

    added ship class & changes to Main

--- Main.java
+++ Main.java
@@ -1,18 +1,44 @@
 import processing.core.PApplet;
 
 public class Main extends PApplet {
+  Ship s;
+  boolean[] keys = new boolean[3];
 
-    public void settings() {
-      size(400, 400);
+  public void settings() {
+    size(1000, 700);
+    s = new Ship(300f, 300f, 0f, 0f, 4.71f, 20f, 2.7f, this);
+    keys[0] = false;
+    keys[1] = false;
+    keys[2] = false;
+  }
+
+  public void draw() {
+    background(255);
+    s.move(keys);
+    s.display();
+  }
+
+public void keyPressed() {
+    int[] dirs = { UP, LEFT, RIGHT };
+    for (int i = 0; i < dirs.length; i++) {
+      if (keyCode == dirs[i]) {
+        keys[i] = true;
+      }
     }
-    public void draw() {
-      background(255);
-      circle(width/2, height/2, 50);
+  }
+
+  public void keyReleased() {
+    int[] dirs = { UP, LEFT, RIGHT };
+    for (int i = 0; i < dirs.length; i++) {
+      if (keyCode == dirs[i]) {
+        keys[i] = false;
+      }
     }
+  }
 
   public static void main(String[] args) {
-    String[] processingArgs = {"MySketch"};
-		Main mySketch = new Main();
-		PApplet.runSketch(processingArgs, mySketch);
+    String[] processingArgs = { "MySketch" };
+    Main mySketch = new Main();
+    PApplet.runSketch(processingArgs, mySketch);
   }
 }
